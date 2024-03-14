17773b626843ee26169d1952dbb58add8235b810
ndaruvala <ndaruvala2346@gmail.com>
2024-03-14 15:58:01 -0400

    Add query 1

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,7 +6,7 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   }
-  if (query.toLowerCase() == "What is your name?") {
+  if (query == "What is your name?") {
     return (
       "Nader"
     );
